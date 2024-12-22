1e14b90e2cd15c5e8a14bdd2e37e47c01f084a39
Mel Bazany <melissa.bazany@mightyinthemidwest.com>
2024-12-22 17:16:53 -0600

    Add Tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,13 @@
 import { defineConfig } from 'astro/config';
 
 import preact from "@astrojs/preact";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://melissabazany.netlify.app/",
-  integrations: [preact()]
+  integrations: [preact(), tailwind({
+    applyBaseStyles: false,
+    nesting: true,
+  })]
 });
