3d7fc23bf82decd516dd4687c5e8a96e3bdc7f34
Mel Bazany <melissa.bazany@mightyinthemidwest.com>
2024-12-30 10:36:53 -0600

    Install Vue

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,13 @@ import { defineConfig } from 'astro/config';
 import preact from "@astrojs/preact";
 import tailwind from "@astrojs/tailwind";
 
+import vue from "@astrojs/vue";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://melissabazany.netlify.app/",
   integrations: [preact(), tailwind({
     applyBaseStyles: false,
     nesting: true,
-  })]
+  }), vue()]
 });
