09d9bcaa40d0594d5f3c88716111096b4e9f1c02
Mel Bazany <melissa.bazany@mightyinthemidwest.com>
2025-01-03 14:40:27 -0600

    Update structure, class names in components/Accordion

--- src/components/Accordion.vue
+++ src/components/Accordion.vue
@@ -1,35 +1,38 @@
 <template>
-    <ul class="c-faqs__accordion-list">
-        <li class="c-faq-item"
-            v-for="item in accordionItems"
-        >
-            <div class="c-faq-item__details">
 
-                <template v-if="item.title != null && item.content != null">
-                    <Disclosure
-                        as="div"
-                        class="c-faq__accordion-item"
-                        v-slot="{ open }"
-                    >
-                        <DisclosureButton
-                            :class="open ? 'c-faq-item__button--is-open' : ''"
-                            class="c-faq-item__button"
+    <div class="c-accordion">
+        <ul class="c-accordion__list">
+            <li class="c-accordion-item"
+                v-for="item in accordionItems"
+            >
+                <div class="c-accordion-item__details">
+                    <template v-if="item.title != null &&( item.listContent != null || item.introText != null)">
+                        <Disclosure
+                            as="div"
+                            class="c-accordion__accordion-item"
+                            v-slot="{ open }"
                         >
-                            <span class="c-faq-item__button-title c-faq-item__button-title--is-mobile">{{item.title}}</span>
-                            <span class="c-faq-item__state-indicator-icon--is-mobile">
-                                {{ open ? '-' : '+' }}
-                            </span>
-                        </DisclosureButton>
-                            <DisclosurePanel
-                                class="c-faq-item__panel"
-                                v-html="convertToList(item.content)"
-                            ></DisclosurePanel>
-                    </Disclosure>
-                </template>
-
-            </div>
-        </li>
-    </ul>
+                            <DisclosureButton
+                                :class="open ? 'c-accordion-item__button--is-open' : ''"
+                                class="c-accordion-item__button"
+                            >
+                                <span class="c-accordion-item__button-title c-accordion-item__button-title">{{item.title}}</span>
+                                <span class="c-accordion-item__state-indicator-icon">
+                                    {{ open ? '-' : '+' }}
+                                </span>
+                            </DisclosureButton>
+                                <DisclosurePanel
+                                    class="c-accordion-item__panel"
+                                >
+                                <div v-if="item.introText != null" v-html="item.introText"></div>
+                                <div v-if="item.listContent != null" v-html="convertToList(item.listContent)"></div>
+                            </DisclosurePanel>
+                        </Disclosure>
+                    </template>
+                </div>
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script>
@@ -59,10 +62,10 @@ export default {
             if (!Array.isArray(accordionItemContent) || !accordionItemContent.length) return '';
 
             const listItems = accordionItemContent
-                .map(item => `<li class="c-faq-item__list-item">${item}</li>`)
+                .map(item => `<li class="c-accordion-item__list-item">${item}</li>`)
                 .join('');
 
-            return `<ul class="c-faq-item__list">${listItems}</ul>`;
+            return `<ul class="c-accordion-item__list">${listItems}</ul>`;
         }
     },
 };
