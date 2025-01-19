5f65fec799d80ea0743b55f94209708068f8fbef
Mel Bazany <melissa.bazany@mightyinthemidwest.com>
2025-01-19 13:23:14 -0600

    Add onEnterBack, onLeave, onLeaveBack ScrollTrigger event listeners

--- src/scripts/ScrollingReveal.js
+++ src/scripts/ScrollingReveal.js
@@ -13,7 +13,7 @@ function ScrollingReveal() {
     // Attach scroller triggers to items and apply settings
     items.forEach((item, index) => {
         let trigger = item;
-        let start = 'top 70%';
+        let start = 'top 50%';
         let scrub = '2';
 
         ScrollTrigger.create({
@@ -22,6 +22,15 @@ function ScrollingReveal() {
             scrub: scrub,
             onEnter: () => {
                 trigger.classList.add('--is-in-view');
+            },
+            onEnterBack: () => {
+                trigger.classList.add('--is-in-view');
+            },
+            onLeave: () => {
+                trigger.classList.remove('--is-in-view');
+            },
+            onLeaveBack: () => {
+                trigger.classList.remove('--is-in-view');
             },
         });
     });
